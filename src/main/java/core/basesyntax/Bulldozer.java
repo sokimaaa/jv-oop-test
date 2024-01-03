a4139ac98a16c8cfee43021cce5b5af7c42bec6b
global source <danmaksimuk@gmail.com>
2024-01-03 16:23:40 +0100

    created classes

---
+++ src/main/java/core/basesyntax/Bulldozer.java
@@ -0,0 +1,11 @@
+package core.basesyntax;
+
+class Bulldozer extends Machine {
+    public void doWork() {
+        System.out.println("Bulldozer started working");
+    }
+
+    public void stopWork() {
+        System.out.println("Bulldozer stopped working");
+    }
+}
